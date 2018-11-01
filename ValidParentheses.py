c67a07485c471b49de6de1c6cd6f922742310861
Wang <wangeri@amazon.com>
2018-11-01 00:06:43 -0700

    ValidParentheses

---
+++ ValidParentheses.py
@@ -0,0 +1,16 @@
+class Solution(object):
+	def isValid(self, s):
+		 """
+        :type s: str
+        :rtype: bool
+        """
+        pars = [None]
+        parmap = {')': '(', '}': '{', ']': '['}
+        for c in s:
+        	if c in parmap:
+        		if parmap[c] != pars.pop():
+        			return False
+        	else:
+        		pars.append(c)
+
+        return len(pars) == 1
