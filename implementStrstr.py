422065135a8de7cd98da5a6e319fd5737b4af96f
Wang <wangeri@amazon.com>
2018-11-01 23:12:48 -0700

    implementStrstr

---
+++ implementStrstr.py
@@ -0,0 +1,20 @@
+class Solution(object):
+	def strStr(self, haystack, needle):
+		"""
+		:type haystack: str
+		:type needle: str
+		:rtype: int
+		"""
+
+		if not needle:
+			return 0;
+
+		for in in range(len(haystack) - len(needle) + 1):
+			if haystack[i] == needle[0]:
+				j = 1;
+				while j < len(needle) and haystack[i+j] == needle[j]
+					j += 1
+				if j == len(needle):
+					return i;
+
+		return -1;
